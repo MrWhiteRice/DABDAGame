d435c138aa0d6088aa65f87a0eabbfbfd8b61bfa
MrWhiteRice <jacob_kotro@hotmail.com>
2019-02-20 23:36:35 +1100

    added health

---
+++ Assets/Health.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+	int health = 100;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.F))
+		{
+			health -= 10;
+		}
+	}
+
+	public int GetHealth()
+	{
+		return health;
+	}
+}
---
+++ Assets/HealthUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+	Text health;
+	Health playerHealth;
+
+	void Start()
+	{
+		playerHealth = FindObjectOfType<Health>();
+		health = GetComponent<Text>();
+	}
+
+    void Update()
+    {
+		health.text = playerHealth.GetHealth().ToString();
+    }
+}
